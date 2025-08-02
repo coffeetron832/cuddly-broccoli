a34672813a2c4a5b61ee16fb2c64da53d6392f8b
coffeetron832 <jpontonh@gmail.com>
2025-08-02 16:19:02 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8, // seguridad mínima
+    minlength: 8,
   },
   joinedAt: {
     type: Date,
@@ -28,13 +28,18 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-// Hashear contraseña antes de guardar
+// Middleware: hashear contraseña antes de guardar
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  const salt = await bcrypt.genSalt(12); // más seguro que 10
+  const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// Método para comparar contraseña ingresada con la hasheada
+UserSchema.methods.comparePassword = async function (passwordIngresada) {
+  return await bcrypt.compare(passwordIngresada, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
