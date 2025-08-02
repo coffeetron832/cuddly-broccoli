da06968ae0ddc7440e05a80fdcf96faa881dcc62
coffeetron832 <jpontonh@gmail.com>
2025-08-02 16:16:11 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,25 +1,40 @@
 // /models/User.js
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    minlength: 3,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
+    minlength: 8, // seguridad mínima
   },
   joinedAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+// Hashear contraseña antes de guardar
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(12); // más seguro que 10
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
