c15906386b0da854b3faf65c085ea49fb972eddc
coffeetron832 <jpontonh@gmail.com>
2025-08-02 16:11:26 -0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,17 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "secreto123";
 
 function verifyToken(req, res, next) {
-  const token = req.headers["authorization"];
-  if (!token) return res.status(401).json({ message: "Token no enviado" });
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Soporta "Bearer <token>"
+
+  if (!token) return res.status(401).json({ type: "no_token", message: "Token no enviado" });
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(403).json({ message: "Token inválido" });
+    return res.status(403).json({ type: "invalid_token", message: "Token inválido" });
   }
 }
 
